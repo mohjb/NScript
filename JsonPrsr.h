9c247a71349c21695bdfffaa740032ae83f07db8
Mohamad Bo-Hamad <mohjb@users.noreply.github.com>
2018-04-18 15:19:02 +0300

    Update JsonPrsr.h

--- JsonPrsr.h
+++ JsonPrsr.h
@@ -1,19 +1,5 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
-
-//array and obj are linkedListEntry
-//enum NTyp{undefined,nul,false,true,num,str,array,obj;}
-
-
 //Node
-
 typedef struct {
     enum { NUL,NUM,STR,ARY,OBJ,FNC } type ;
     struct{N*n;N*x;N*refVis}inbound;
@@ -49,15 +35,15 @@ typedef struct {
     };
 } N;
 /* C-Lang functions
-init (num, str, ary, obj, fnc)
-assignMember(num, str, ary, obj, fnc)
+init (forEach num, str, ary, obj, fnc)
+assignMember(forEach num, str, ary, obj, fnc)
 dispose/deleteMember
 equals,lt,le,gt,ge
 toString/toJsonString
-concat (str,ary,obj)
-splice (str,ary,obj)
-indexOf (str,ary,obj)
-at (str,ary,obj)
+concat (forEach str,ary,obj)
+splice (forEach str,ary,obj)
+indexOf (forEach str,ary,obj)
+at (forEach str,ary,obj)
 str:
 	startsWith
 	endsWith
@@ -158,6 +144,22 @@ Knowledge
 */
 
 struct StrBuff{uint n,i,j;char*buf;StrBuff* src,dst;FILE*fp;}
+/*The StringBuffer
+	holds the buf=malloc(n), and i,j are offsets of the ready chars, where i,j go circular
+	src and dst are like a linkedlist, for all the StrBuf that share the same buf mem-address,
+	but each has different i,j offsets, in the cases of substring
+	
+	but as for the function trim, it would possibly allocated a new address 
+	, in such cases the struct would get removed from the src,dst linkedlist
+	
+	when the dispose function is called , it would free the buf 
+	, unless there is a src,dst linkedlist, because the buf is shared
+	
+	this struct can play also a role of a InputStream 
+	when initialized with a fileName or a File pointer
+	
+	
+*/
 
 StrBuff*StrBuff_init(int n){}
 
@@ -172,7 +174,6 @@ uint StrBuff_length(StrBuff*t){}
 char StrBuff_shift(StrBuff*t){}
 char StrBuff_unshift(StrBuff*t,char p){}
 char StrBuff_push(StrBuff*t,char p){return 0;}
-//StrBuff*StrBuff_splice(StrBuff*t,uint offset,uint len, uint srcLen,char*src,uint srcOffset){return t;}
 StrBuff*StrBuff_concat(StrBuff*t, uint srcLen,char*src){return t;}
 StrBuff*StrBuff_append(StrBuff*t, StrBuff*p){return t;}
 StrBuff*StrBuff_substring(StrBuff*t,uint offset,uint len){return t;}
@@ -184,26 +185,25 @@ json parser that generates struct-N
 */
 struct JsonPrsr{
 	StrBuff*buf,comment;
-	char c;uint _row,_col;
-	N*cache;}
+	char c,peek;uint _row,_col;}//N*cache;
 
-JsonPrsr*JsonPrsr_read(JsonPrsr*t){return t;}
+char JsonPrsr_read(JsonPrsr*t){return t;}
 
 JsonPrsr*JsonPrsr_init(StrBuff*b){return 0;}
 JsonPrsr*JsonPrsr_init_fn(char*fileName){return 0;}
 JsonPrsr*JsonPrsr_init_str(char*p,uint n){return 0;}
 N*JsonPrsr_parse(JsonPrsr*t){return 0;}
+N* JsonPrsr_parseItem(JsonPrsr*t){}
+N* JsonPrsr_extractStringLiteral(JsonPrsr*t){}
+N* JsonPrsr_extractIdentifier(JsonPrsr*t){}
+N* JsonPrsr_extractDigits(JsonPrsr*t){}
+N* JsonPrsr_extractArray(JsonPrsr*t){}
+N* JsonPrsr_extractObject(JsonPrsr*t){}
 void JsonPrsr_skipRWS(JsonPrsr*t){}
 void JsonPrsr_skipRWSx(JsonPrsr*t){}
-void JsonPrsr_parseItem(JsonPrsr*t){}
-void JsonPrsr_extractStringLiteral(JsonPrsr*t){}
-void JsonPrsr_extractIdentifier(JsonPrsr*t){}
-void JsonPrsr_extractDigits(JsonPrsr*t){}
-void JsonPrsr_extractArray(JsonPrsr*t){}
-void JsonPrsr_extractObject(JsonPrsr*t){}
 void JsonPrsr_skipWhiteSpace(JsonPrsr*t){}
 void JsonPrsr_skipComments(JsonPrsr*t){}
-void JsonPrsr_peek(JsonPrsr*t){}
+char JsonPrsr_peek(JsonPrsr*t){}
 void JsonPrsr_rc(JsonPrsr*t){}
 void JsonPrsr_nlRC(JsonPrsr*t){}
 void JsonPrsr_incCol(JsonPrsr*t){}
@@ -216,39 +216,6 @@ void JsonPrsr_buff(JsonPrsr*t){}
 void JsonPrsr_buffC(JsonPrsr*t,char c){}
 StrBuff*JsonPrsr_consume(JsonPrsr*t){return 0;}
 void JsonPrsr_consume_to_N(JsonPrsr*t,N*p){}
-
-/*
-void skipRWS
-void skipRWSx(...)
-Object parse()
-Object parseItem()
-String extractStringLiteral()
-Object extractIdentifier()
-Object extractDigits()
-List<Object> extractArray()
-Map<Object,Object> extractObject()
-void skipWhiteSpace()
-boolean skipComments()
-
-
-char read()//read a char from the rdr
-
-char peek()
-
-int _row,_col;String rc()
-
-void nlRC(){_col=1;_row++;}
-void incCol(){_col++;}
-char setEof()
-char nxt(char h)
-char bNxt()
-char nxt()
-String next(int n)
-char buff()
-buff(char p)
-String consume()
-
-*/
 
 int main()
 {
